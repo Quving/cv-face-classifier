722687c56b7a589691f6ad7b7cbeef94389cecc6
Vinh Ngu <vinh-ngu@hotmail.com>
2018-02-25 06:19:06 +0100

    Initialize cnn/.

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-mkdir -p use-vgg16/training
-mkdir -p use-vgg16/bottleneck_features
-mkdir -p use-vgg16/models
-mkdir -p use-vgg16/class_indices
-
-
-mkdir -p from-scratch/training
-mkdir -p from-scratch/bottleneck_features
-mkdir -p from-scratch/models
-mkdir -p from-scratch/class_indices
-
+mkdir -p cnn/training
+mkdir -p cnn/bottleneck_features
+mkdir -p cnn/models
+mkdir -p cnn/class_indices
 
