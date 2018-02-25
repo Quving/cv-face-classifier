6404eff728db785904d35b645c170978982f08d2
Vinh Ngu <vinh-ngu@hotmail.com>
2018-02-25 22:48:12 +0100

    Add initialize script.

---
+++ initialize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p use-vgg16/training
+mkdir -p use-vgg16/bottleneck_features
+mkdir -p use-vgg16/models
+mkdir -p use-vgg16/class_indices
+
+
+mkdir -p from-scratch/training
+mkdir -p from-scratch/bottleneck_features
+mkdir -p from-scratch/models
+mkdir -p from-scratch/class_indices
+
+
