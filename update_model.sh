5000148e64b04a36976ebee1087e736e4c8b2589
Vinh Ngu <vinh-ngu@hotmail.com>
2018-03-03 18:15:59 +0100

    Remove unnecessary script.

--- update_model.sh
+++
@@ -1,7 +0,0 @@
-set -e
-nc_path="$HOME"/Nextcloud/github/cv-face-classifier/
-rsync -r -v tams223:/homeL/project2017/2ngu/cv-face-classifier/cnn/models "$nc_path"
-rsync -r -v tams223:/homeL/project2017/2ngu/cv-face-classifier/cnn/class_indices "$nc_path"
-
-cd $nc_path && zip -r models.zip models
-cd $nc_path && zip -r class_indices.zip class_indices
