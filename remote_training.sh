1d352996b5e0eee1ff7b6dc8f67e335e140095c4
Vinh Ngu <v.ngu@mmkh.de>
2018-03-09 18:22:01 +0100

    Remove old files before updating.

--- remote_training.sh
+++ remote_training.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+set -e
 ssh tams225 'bash -c "bash automation/update_model.sh"'
+echo "Update model locally."
+
+rm -rf cnn/models cnn/training cnn/class_indices cnn/bottleneck_features
 
 wget $NC_URL/index.php/s/$NC_MODEL_TOKEN/download && \
     unzip download -d cnn && \
@@ -13,3 +16,5 @@ wget $NC_URL/index.php/s/$NC_TRAINING_TOKEN/download && \
 wget $NC_URL/index.php/s/$NC_CLASS_INDICES_TOKEN/download && \
     unzip download -d cnn && \
     rm download
+
+echo "Job finished."
