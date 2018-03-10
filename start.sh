26aa084219813b7b1b3f0781266aa3f002bd9970
Vinh Ngu <vinh-ngu@hotmail.com>
2018-03-10 23:54:01 +0100

    Add remove downloads.* at the beginning.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [[ "$UPDATE_WEIGHTS" -eq "1" ]]; then
     NC_CLASS_INDICES_TOKEN='iL3xgnYGyCrw9zM'
     NC_URL='http://nextcloud.quving.com'
 
-    rm -rf cnn/models cnn/training cnn/class_indices cnn/bottleneck_features
+    rm -rf cnn/models cnn/training cnn/class_indices cnn/bottleneck_features download.*
 
     wget $NC_URL/index.php/s/$NC_MODEL_TOKEN/download && \
         unzip download -d cnn && \
